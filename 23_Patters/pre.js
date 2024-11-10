b1870338aa93647ad6b9bf6aa5fe17393d6e0bec
李辰 <2095055676@qq.com>
2024-11-10 22:55:19 +0800

    feat: 23种设计模式: 创建者/工厂模式

---
+++ 23_Patters/pre.js
@@ -0,0 +1,8 @@
+/**
+ * 设计模式原则:
+ * S-Single Responsibility Principle 单一职责原则
+ * O-Open Closed Principle 开放封闭原则
+ * L-Liskov Substitution Principle 里氏替换原则
+ * I-Interface Segregation Principle 接口隔离原则
+ * D-Dependency Inversion Principle 依赖倒置原则
+ */
